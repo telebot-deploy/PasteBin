5930b8fdfa8f2eb898d75b0f4dc851d3f3959a74
telebot-deploy <contactushideonline@gmail.com>
2026-08-01 23:07:49 +0530

    Create SQLite instance directory automatically

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
-    SQLALCHEMY_DATABASE_URI = (
-        "sqlite:///" + os.path.join(BASE_DIR, "instance", "paste.db")
-    )
+    INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+    os.makedirs(INSTANCE_DIR, exist_ok=True)
+
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(INSTANCE_DIR, "paste.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
